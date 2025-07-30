405daa8fdd87694fbdc5186101ce06d13d3dfe6f
hong <hong@sesac.com>
2025-07-30 17:15:14 +0900

    library app 생성

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Post
+# Create your views here
 
-# Create your views here.
+
+def index(request):
+    posts=Post.objects.all()
+    return render(request,
+                  'blog/index.html',
+                  context={'posts':posts}
+                  )
+def detail(request, pk):
+    post=Post.objects.get(pk=pk)
+    return render(request,
+                  'blog/detail.html',
+                  context={'post2':post})
